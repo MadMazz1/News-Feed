e9c41ecb5fbad246ab34aec4f3803a3ff2730f6d
Mazz <kmazzone93@gmail.com>
2023-02-27 11:56:45 -0500

    Made things look a little neater/nicer.
    TODO:
    Fix win32tty output.

--- daNews.py
+++ daNews.py
@@ -3,12 +3,12 @@ from colorama import Fore
 
 '''
 -=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-MazZ's News Hub-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-This program uses the 'currents' API which keeps track of current/old News articles from a list of hundreds of reliable 
-news outlets. Including but not limited to: FOX, CNN, ABC, NBC, MSNBC, NPR, AP, BuzzFeed, Washington Post, NY Times, BBC,
-SCMP, ADN, CDN, WWLP, TheRegister, SMH, WBBJTV, TMZ, WSVN, WNBC, and MUCH MUCH MORE!
+This program uses the 'currents' API which keeps track of current/old News articles from a list of over 42,000 
+news outlets. Including but not limited to: FOX, CNN, ABC, NBC, MSNBC, NPR, AP, CNBC, FT, BuzzFeed, Washington Post, 
+NY Times, BBC, SCMP, ADN, CDN, WWLP, TheRegister, SMH, WBBJTV, TMZ, WSVN, WNBC, WSWS, EWN, and MANY, MANY MORE!
 
-You can see all the latest articles that have been published, from various News agencies. Or search for something 
-specific using keyword(s)/phrases. 
+You can see all the latest articles that have been published, from these various News agencies, in realtime. Or search 
+for something specific using keyword(s)/phrases. 
 
 Rather than search through 500 different news outlets, the user can simply use this hub to search through all of them
 within a minute.
@@ -18,14 +18,14 @@ https://currentsapi.services/en
 - Register account (Free)
 - You'll be given a free API key which allows for up to 600 requests per day.
     - If not self-evident >> Replace API_KEY with your own >> Run Program >> VOILA!
-    
+
 Note:
 If you're running this in a Win32 tty session, the colors may mess with the output since the tty itself does not seem to
-read ANSI chars. I am currently working on a fix for this.
+read the ANSI chars from the color lib 'colorama >>> Fore'. I'll have a fix for this in the future.
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 '''
 
-API_KEY = 'ADD YOUR API KEY HERE'
+API_KEY = 'YOUR API KEY GOES HERE'
 
 api = CurrentsAPI(api_key=API_KEY)
 
@@ -123,40 +123,47 @@ def search_news(keyword=None):
 def main():
     run = False
     while not run:
-        choice = input("Would you like to search for something specific? [Y/N]\n"
-                       "(If No >> You will be given a list of the Latest News Articles!): ").lower()
+        choice = input(Fore.GREEN + "~*~*~*~*~*~*~Welcome to MazZ\'s News Media Hub!~*~*~*~*~*~*~*~\n" +
+                       Fore.MAGENTA + "Please Choose an Option:\n " +
+                       Fore.BLUE + "[1] Search for Specific Topic\n "
+                                   "[2] Return Most Recently Published Articles\n" +
+                       Fore.MAGENTA + "Choice: " + Fore.RESET).lower()
 
-        if choice == 'y':
-            search = input("Type Keyword or name of Article to search for: ")
+        if choice == str(int(1)):
+            search = input(Fore.BLUE + "Type Keyword or name of Article to search for: " + Fore.RESET).lower()
             print(Fore.GREEN + "Searching..." + Fore.RESET)
 
             search_news(keyword=search)
 
-            cont = input("Would you like to search for something else? [Y/N]: ").lower()
+            cont = input(Fore.BLUE + "Would you like to search for something else? [Y/N]: " + Fore.RESET).lower()
 
             if cont == 'y':
                 continue
             else:
                 quit()
 
-        elif choice == 'n':
+        elif choice == str(int(2)):
+            print(Fore.GREEN + 'Searching...' + Fore.RESET)
             current_news()
 
-            cont2 = input("Would you like to search for a specific topic now? [Y/N] - 'N' to quit. "
-                          "Or 'R' for refresh....").lower()
+            cont2 = input(Fore.MAGENTA + "Please Choose an Option:\n" + Fore.BLUE +
+                          " [1] Search for Specific Article, Keyword(s), or Phrase.\n"
+                          " [2] Refresh Current\n"
+                          " [3] Quit\n" + Fore.MAGENTA +
+                          "Choice: " + Fore.RESET).lower()
 
-            if cont2 == 'y':
-                phrase = input("What News Topic would you like to search for?: ")
+            if cont2 == str(int(1)):
+                phrase = input(Fore.BLUE + "What News Topic would you like to search for?: " + Fore.RESET).lower()
                 search_news(keyword=phrase)
 
-            elif cont2 == 'r':
+            elif cont2 == str(int(2)):
                 current_news()
 
-            elif cont2 == 'n':
+            elif cont2 == str(int(3)):
                 quit()
 
         else:
-            print("Please answer with Y(Yes) or N(No).")
+            print(Fore.RED + "Please choose an option..." + Fore.RESET)
 
 
 if __name__ == '__main__':
